6ee5830790f8b6cec1e6ed8b885da41a2ffa81af
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-09-18 19:14:29 +0800

    integrate discount calculation logic into rental creation process (#4)

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,23 +1,27 @@
-from pydantic import BaseModel, Field, NaiveDatetime
+import datetime
+
+from pydantic import BaseModel, Field, NaiveDatetime, AwareDatetime
+
 
 class Rental(BaseModel):
     id: str = Field(..., description="The unique identifier for the customer")
     customer_id: str = Field(..., description="The customer ID")
-    rental_date: NaiveDatetime = Field(..., description="The date when the rental was made")
+    rental_date: AwareDatetime = Field(..., description="The date when the rental was made")
     shoe_size: float = Field(..., ge=0, description="The shoe size of the customer, must be non-negative")
     rental_fee: float = Field(..., ge=0, description="The rental fee, must be non-negative")
     discount: float = Field(0, ge=0, description="The discount applied to the rental fee, must be non-negative")
     total_fee: float = Field(..., ge=0, description="The total fee after discount, must be non-negative")
 
 
-class RentalCreate(BaseModel):
+class RentalCreateParameters(BaseModel):
     customer_id: str
-    rental_date: NaiveDatetime
+    rental_date: datetime.date
     shoe_size: float
     rental_fee: float
-    discount: float = 0
-    total_fee: float
 
+class RentalCreate(RentalCreateParameters):
+    discount: float
+    total_fee: float
 
 class RentalResponse(RentalCreate):
     id: str
@@ -26,4 +30,4 @@ class GetRentalsResponse(BaseModel):
     rentals: list[RentalResponse]
     
 class GetRentalResponse(BaseModel):
-    rental: RentalResponse
+    rental: Rental
